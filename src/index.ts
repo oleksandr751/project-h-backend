4db365062ca2963601dcf05a079e72bf415dd5e3
Oleksandr Pecheniuk <oleksandr@ai-omatic.com>
2023-02-15 11:13:58 +0100

    edit: env variables added

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import { MONGO_URI } from "./config/index";
 const app = express();
 const cors = require("cors");
 const port = process.env.PORT;
+const mongoUri = process.env.MONGO_URI;
 app.use(cors());
 app.use(express.json({ limit: "500mb" }));
 app.use(express.urlencoded({ extended: true }));
@@ -13,10 +14,10 @@ app.use("/api/tags", require("./routes/tags.routes"));
 app.use("/api/chartValues", require("./routes/values.routes"));
 app.use("/api/auth", require("./routes/users.routes"));
 
-const PORT = process.env.PORT || port || 5000;
+const PORT = port || 5000;
 async function start() {
   try {
-    await mongoose.connect(MONGO_URI);
+    await mongoose.connect(mongoUri ?? MONGO_URI);
     app.listen(PORT, () => console.log(`App has been started on ${PORT}`));
   } catch (e: any) {
     console.log(e.message);
